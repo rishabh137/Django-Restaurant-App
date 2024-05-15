908287f2902cbbf2048d0aadc1aba460edfdeef4
Rishabh Raj <rishabhraj13951@gmail.com>
2024-05-15 23:15:10 +0530

    remove vercel setting

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -180,6 +180,3 @@ STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
-
-# STATIC_URL = "static/"
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
